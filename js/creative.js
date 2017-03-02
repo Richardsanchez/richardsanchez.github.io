73ac8decaf93de3dbc87128ad9fd7c77c555422d
Richard Sanchez <richardsanchez.me@gmail.com>
2017-03-02 18:33:53 -0500

    Add files via upload

--- js/creative.js
+++ js/creative.js
@@ -23,7 +23,7 @@
         offset: 51
     })
 
-
+ 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
